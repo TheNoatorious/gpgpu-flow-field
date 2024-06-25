8243f8fc1ffcbc05fbb4d9f4344aab58a5500f54
N.Z. Timisela <noatimisela@gmail.com>
2024-06-25 18:28:06 +0200

    Animate the particles with a 4D simplex noise

--- src/shaders/gpgpu/particles.glsl
+++ src/shaders/gpgpu/particles.glsl
@@ -1,6 +1,23 @@
+uniform float uTime; 
+
+#include ../includes/simplexNoise4d.glsl
+
 void main() {
+    float time = uTime * 0.2;
     vec2 uv = gl_FragCoord.xy / resolution.xy;
     vec4 particle = texture(uParticles, uv); // 3D position as rgb colours
+
+	// Particle flow field direction
+    // A SimplexNoise for each vertex
+    vec3 flowField = vec3(
+		simplexNoise4d(vec4(particle.xyz + 0.0, time)),
+		simplexNoise4d(vec4(particle.xyz + 1.0, time)),
+		simplexNoise4d(vec4(particle.xyz + 2.0, time))
+    ); 
+
+    flowField = normalize(flowField); // Normalize the direction
+    particle.xyz += flowField * 0.01; // Add the direction to the particles
+
     
     gl_FragColor = particle;
 }
