4593f0900803b1695c5b537e0ed3dd4623962190
N.Z. Timisela <noatimisela@gmail.com>
2024-06-25 15:58:02 +0200

    Load model instead of geometry

--- src/script.js
+++ src/script.js
@@ -89,6 +89,12 @@ debugObject.clearColor = "#29191f";
 renderer.setClearColor(debugObject.clearColor);
 
 /**
+ * Load model
+ */
+
+const gltf = await gltfLoader.loadAsync("./model.glb");
+
+/**
  * Base geometry
  */
 const baseGeometry = {};
