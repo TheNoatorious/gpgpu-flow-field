93624de2f8698f9aeec1d4785317193cb52076a7
N.Z. Timisela <noatimisela@gmail.com>
2024-06-25 16:19:01 +0200

    Remove animation on the y-coordinate

--- src/shaders/gpgpu/particles.glsl
+++ src/shaders/gpgpu/particles.glsl
@@ -1,7 +1,6 @@
 void main() {
     vec2 uv = gl_FragCoord.xy / resolution.xy;
     vec4 particle = texture(uParticles, uv); // 3D position as rgb colours
-    particle.y += 0.01;
     
     gl_FragColor = particle;
 }
